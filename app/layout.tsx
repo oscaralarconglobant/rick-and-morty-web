612821192fb947deb24971b740ca90f3d315801f
Oscar Alarcon <oalarcon.contractor@luminatedata.com>
2024-04-26 15:44:01 -0600

    Home Page

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Rick And Morty",
+  description: "Oscar Alarcón Rodríguez",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="shortcut icon" href="/rick.ico"></link>
+      </head>
+
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { CharacterItem, InfiniteList, Navbar } from "./components";
+import { useGetCharacters } from "./hooks";
+import { ICharacter } from "./interfaces";
+import { PageContainer } from "./styles";
+
+export default function Home() {
+  const [getCharacters] = useGetCharacters();
+
+  return (
+    <PageContainer>
+      <Navbar hasSearch/>
+      <InfiniteList<ICharacter>
+        getData={getCharacters}
+        component={(item) => <CharacterItem key={item.id} item={item} />}
+      />
+    </PageContainer>
+  );
+}
